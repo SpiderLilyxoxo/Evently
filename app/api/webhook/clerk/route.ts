615c2821f7ad1d23f6a54c01779dc12aa9770bc5
SpiderLilyxoxo <spiderlily.wm@gmail.com>
2024-01-11 02:09:29 +0300

    Adding Events and show them on Main page

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -54,28 +54,29 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
  
-  if (eventType === 'user.created') {
-	const {id, email_addresses, image_url, first_name, last_name, username } = evt.data
-	const user =  {
-		clerkId: id,
-		email: email_addresses[0].email_address,
-		username: username!,
-		firstName: first_name,
-		lastName: last_name,
-		photo: image_url,
-	}
+  if(eventType === 'user.created') {
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
-	const newUser =  await createUser(user)
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
+    }
 
-	if (newUser) {
-		await clerkClient.users.updateUserMetadata(id, {
-			publicMetadata: {
-				userId: newUser._id
-			}
-		})
-	}
+    const newUser = await createUser(user);
 
-	return NextResponse.json({message: 'OK', user: newUser})
+    if(newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id
+        }
+      })
+    }
+
+    return NextResponse.json({ message: 'OK', user: newUser })
   }
 
   if (eventType === 'user.updated') {
